a2101449975d52d18c9b14cdf23836c4088e6ac7
ktritsh7 <ktriesh7@gmail.com>
2021-07-01 20:43:28 +0530

    Password validation testcase atleast one number

--- src/main/java/com/bridgelabz/userregistrationmain/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/userregistrationmain/UserRegistrationMain.java
@@ -44,7 +44,7 @@ public class UserRegistrationMain {
     }
 
     public boolean passwordValidate() {
-        System.out.print("Enter your Password to validate : ");
+        System.out.print("Enter your Password : ");
         String password = scanner.next();
         System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(?=.*[0-9]).{8,}$", password));
         return false;
