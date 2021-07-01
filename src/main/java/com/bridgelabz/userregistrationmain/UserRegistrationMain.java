c8af6294de099fcd25288dc26c5c5538ea2bf904
ktritsh7 <ktriesh7@gmail.com>
2021-07-01 19:12:51 +0530

    first name validation testcase

--- src/main/java/com/bridgelabz/userregistrationmain/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/userregistrationmain/UserRegistrationMain.java
@@ -1,7 +1,20 @@
 package com.bridgelabz.userregistrationmain;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistrationMain {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Testcase");
+        UserRegistrationMain userRegistrationMain = new UserRegistrationMain();
+        userRegistrationMain.firstNameValidate();
+    }
+
+    public boolean firstNameValidate() {
+        System.out.print("Enter your first name : ");
+        String firstName = scanner.next();
+        System.out.println(Pattern.matches("^[A-Z][a-z]{2,}", firstName));
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/userregistrationtest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationtest/UserRegistrationTest.java
@@ -1,4 +1,15 @@
 package com.bridgelabz.userregistrationtest;
 
+import com.bridgelabz.userregistrationmain.UserRegistrationMain;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class UserRegistrationTest {
+
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue(){
+        UserRegistrationMain userRegistrationMain = new UserRegistrationMain();
+        boolean result = userRegistrationMain.firstNameValidate();
+        Assertions.assertTrue(result);
+    }
 }
